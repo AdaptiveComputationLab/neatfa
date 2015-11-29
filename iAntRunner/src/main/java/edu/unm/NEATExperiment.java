de19fc4fbc97736cd62624fa4a31137c1330ff6f
John Ericksen <johncarl81@gmail.com>
2015-11-29 14:45:54 -0700

    Added NEAT configuration from example Reconfigured iAnt.xml to allow for larger populations

--- iAntRunner/src/main/java/edu/unm/NEATExperiment.java
+++ iAntRunner/src/main/java/edu/unm/NEATExperiment.java
@@ -38,16 +38,40 @@ public class NEATExperiment {
         Neat.initbase();
 
         Neat.p_pop_size = populationSize;
-        Neat.p_survival_thresh = 0.1;
 
+        Neat.p_trait_param_mut_prob = 0.5;
+        Neat.p_trait_mutation_power = 1.0;
+        Neat.p_linktrait_mut_sig = 1.0;
+        Neat.p_nodetrait_mut_sig = 0.5;
+        Neat.p_weight_mut_power = 2.5;
+        Neat.p_recur_prob = 0.1;
+        Neat.p_disjoint_coeff = 1.0;
+        Neat.p_excess_coeff = 1.0;
+        Neat.p_mutdiff_coeff = 0.4;
+        Neat.p_compat_threshold = 3.0;
+        Neat.p_age_significance = 1.0;
+        Neat.p_survival_thresh = 0.2;
+        Neat.p_mutate_only_prob = 0.25;
         Neat.p_mutate_random_trait_prob = 0.1;
         Neat.p_mutate_link_trait_prob = 0.1;
         Neat.p_mutate_node_trait_prob = 0.1;
-        Neat.p_mutate_link_weights_prob = 0.1;
-        Neat.p_mutate_toggle_enable_prob = 0.05;
-        Neat.p_mutate_gene_reenable_prob = 0.05;
-
-        Neat.p_weight_mut_power = 1;
+        Neat.p_mutate_link_weights_prob = 0.9;
+        Neat.p_mutate_toggle_enable_prob = 0.0;
+        Neat.p_mutate_gene_reenable_prob = 0.0;
+        Neat.p_mutate_add_node_prob = 0.03;
+        Neat.p_mutate_add_link_prob = 0.08;
+        Neat.p_interspecies_mate_rate = 0.0010;
+        Neat.p_mate_multipoint_prob = 0.3;
+        Neat.p_mate_multipoint_avg_prob = 0.3;
+        Neat.p_mate_singlepoint_prob = 0.3;
+        Neat.p_mate_only_prob = 0.2;
+        Neat.p_recur_only_prob = 0.0;;
+        Neat.p_dropoff_age = 50;
+        Neat.p_newlink_tries = 50;
+        Neat.p_print_every=  10;
+        Neat.p_babies_stolen = 0;
+        Neat.p_num_runs = 1;
+        Neat.p_num_trait_params = 8;
 
         Population population = new Population(Neat.p_pop_size, 12, 3, 5, true, 1);
 
