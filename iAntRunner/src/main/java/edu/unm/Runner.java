97677a84e58e113fe27c35df599aa8a7d17450ae
John Ericksen <johncarl81@gmail.com>
2015-11-29 15:17:01 -0700

    Added loop parameters for entityCount and distributions

--- iAntRunner/src/main/java/edu/unm/Runner.java
+++ iAntRunner/src/main/java/edu/unm/Runner.java
@@ -11,30 +11,33 @@ public class Runner {
     public static void main(String[] args) throws Exception {
 
         int populationSize = 100;
-        int runtime = 3600;
-        int entityCount = 6;
+        int runtime = 10 * 60;
         int localRunnerCount = Runtime.getRuntime().availableProcessors();
         int remoteRunnerCount = 0;
+        int[] entityCounts = {10, 25, 60};
+        int[] distributions = {0, 1, 2};
 
         List<OrganismExecutor> executors = new ArrayList<OrganismExecutor>();
 
-        for(int distribution = 0; distribution < 3; distribution++) {
-            long startTime = System.currentTimeMillis();
-            Logger log = new Logger(startTime);
+        for(int entityCount : entityCounts) {
+            for (int distribution : distributions) {
+                long startTime = System.currentTimeMillis();
+                Logger log = new Logger(startTime);
 
-            //setup local runners
-            for (int i = 0; i < localRunnerCount; i++) {
-                executors.add(new LocalOrganismExecutor("/home/john/dev/iAnt-ARGoS/", log, startTime));
+                //setup local runners
+                for (int i = 0; i < localRunnerCount; i++) {
+                    executors.add(new LocalOrganismExecutor("/home/john/dev/iAnt-ARGoS/", log, startTime));
+                }
+
+                //setup remote runners
+                for (int i = 0; i < remoteRunnerCount; i++) {
+                    executors.add(new RemoteOrganismExecutor("johncarl@maricopa.cs.unm.edu", "~/iAnt-ARGOS;", log, startTime));
+                }
+
+                NEATExperiment experiment = new NEATExperiment(executors, log, populationSize, runtime, distribution, entityCount);
+
+                experiment.run();
             }
-
-            //setup remote runners
-            for (int i = 0; i < remoteRunnerCount; i++) {
-                executors.add(new RemoteOrganismExecutor("johncarl@maricopa.cs.unm.edu", "~/iAnt-ARGOS;", log, startTime));
-            }
-
-            NEATExperiment experiment = new NEATExperiment(executors, log, populationSize, runtime, distribution, entityCount);
-
-            experiment.run();
         }
 
     }
